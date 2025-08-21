d9f512407441e406a1f3b7d498e4afb2d90a2655
Rahul-bot-star-sudo <rahulshinde2577@gmail.com>
2025-08-21 10:45:25 +0530

    today i create request dispatch using include(), forword() and also i create welcome file list

--- servlet-form/src/java/com/packages/RegisterServlet.java
+++ servlet-form/src/java/com/packages/RegisterServlet.java
@@ -1,39 +1,44 @@
 package com.packages;
 
 import java.io.IOException;
+import javax.servlet.RequestDispatcher;
 import java.io.PrintWriter;
 import javax.servlet.*;
 import javax.servlet.http.*;
-
 public class RegisterServlet extends HttpServlet {
-  public void doPost(HttpServletRequest req,HttpServletResponse res)throws ServletException, IOException{
-      res.setContentType("text/html");
-      PrintWriter out = res.getWriter();
-      out.println("<h1>Wellcome to Register servlet!</h1>");
-      String name = req.getParameter("user_name");
-      String password = req.getParameter("user_password");
-      String email = req.getParameter("user_email");
-      String gender = req.getParameter("user_gender");
-      String course = req.getParameter("user_course");
-      String cond = req.getParameter("condition");
-      if(cond!=null){
-      if (cond.equals("checked")){
-          out.println("<h2>Name: "+name+"</h2>");
-          out.println("<h2>Password: "+password+"</h2>");
-          out.println("<h2>Gender: "+gender+"</h2>");
-          out.println("<h2>Email: "+email+"</h2>");
-          out.println("<h2>Course: "+course+"</h2>");
-          
-      }
-      else{
-          out.println("<h2>First fill all form and check button from first else</h2>");
-      }
-      }
-      else
-      {
-        out.println("<h2>First fill all form and check button</h2>");
+
+    public void doPost(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+        res.setContentType("text/html");
+        PrintWriter out = res.getWriter();
+        out.println("<h1>Wellcome to Register servlet!</h1>");
+        String name = req.getParameter("user_name");
+        String password = req.getParameter("user_password");
+        String email = req.getParameter("user_email");
+        String gender = req.getParameter("user_gender");
+        String course = req.getParameter("user_course");
+        String cond = req.getParameter("condition");
+        if (cond != null) {
+            if (cond.equals("checked")) {
+                out.println("<h2>Name: " + name + "</h2>");
+                out.println("<h2>Password: " + password + "</h2>");
+                out.println("<h2>Gender: " + gender + "</h2>");
+                out.println("<h2>Email: " + email + "</h2>");
+                out.println("<h2>Course: " + course + "</h2>");
+                
+                RequestDispatcher rd = req.getRequestDispatcher("success");
+                rd.forward(req, res);
+            } else {
+                out.println("<h2>First fill all form and check button from first else</h2>");
+            }
+        } else {
+            out.println("<h2>First fill all form and check button</h2>");
+            // I want to include my index.html output 
+            // Get object of  request disptcher 
          
-      }
-  
-   }
+          RequestDispatcher rd = req.getRequestDispatcher("index.html");
+        rd.include(req, res);
+
+        }
+
+    }
 }
